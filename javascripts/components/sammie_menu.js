2a15c5bfaa94c78853d9fe244df6377a3c6e45ef
Greg Stephen <GregAStephen@gmail.com>
2019-04-12 18:59:00 -0500

    WIP

--- javascripts/components/sammie_menu.js
+++ javascripts/components/sammie_menu.js
@@ -1,21 +1,28 @@
 import meat from '../components/meat.js';
+import veggies from '../components/veggie.js';
+import cheese from '../components/cheese.js';
+import bread from '../components/bread.js';
 import util from '../helpers/util.js';
 import events from '../helpers/eventHandlers.js';
 
 const makeMenu = () => {
   const meatObj = meat.getMeat();
   const meatInfo = Object.entries(meatObj)
-  let domString = '<div class="row col-3 meat-header"><h2>CHECK OUT OUR MEATS</h2>';
+  const veggieObj = veggies.getVeggie();
+  const veggieInfo = Object.entries(veggieObj)
+  const cheeseObj = cheese.getCheese();
+  const cheeseInfo = Object.entries(cheeseObj)
+  const breadObj = bread.getBread();
+  const breadInfo = Object.entries(breadObj)
+  let domString = '';
   for (const [meat, price] of meatInfo) {
-    domString += `<div class="menuCheckbox col-12">`
+    domString += `<div class="menuCheckbox">`
     domString += `<input type="checkbox" name="meat" id="${meat}" value="${meat}">`
-    domString += `<label for="${meat}">${meat}: ${price}</label>`
+    domString += `<label for="${meat}">${meat}: $${price}</label>`
     domString += `</div>`
   }
-  domString += `</div>`
-  util.printToDom('menu-container', domString);
+  util.printToDom('meats', domString);
   events.attachEvents();
-
 };
 
 export default { makeMenu };
--- javascripts/helpers/eventHandlers.js
+++ javascripts/helpers/eventHandlers.js
@@ -1,18 +1,28 @@
-const meatCheckboxes = document.getElementsByName('meat');
+import util from './util.js';
 
-const meatCheckboxChange = (e) => {
+const meatCheckboxes = document.getElementsByName('meat');
+let ingredientList = [];
+
+
+const checkboxChange = (e) => {
   let checkbox = e.target;
   let checkboxId = e.target.id;
+
   if (checkbox.checked) {
-    console.log(`${checkboxId} box was checked`);
+    ingredientList.push(checkboxId);
+    util.printToDom('finished-sandwich-div', ingredientList);
   } else {
-    console.log(`${checkboxId} box was unchecked`);
+    let index = ingredientList.indexOf(checkboxId);
+    if (index !== -1) {
+      ingredientList.splice(index, 1);
+    }
+    util.printToDom('finished-sandwich-div', ingredientList);
   }
-}
+};
 
 const attachEvents = () => {
   for (const meatCheckbox of meatCheckboxes) {
-    meatCheckbox.addEventListener('change', meatCheckboxChange);
+    meatCheckbox.addEventListener('change', checkboxChange);
   }
 }
 
