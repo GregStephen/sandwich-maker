81e042d62a6ed756c558c1cf58ac9db46a73cb4b
Greg Stephen <GregAStephen@gmail.com>
2019-04-12 17:21:05 -0500

    added components

---
+++ javascripts/components/bread.js
@@ -0,0 +1,13 @@
+const bread = {
+  white: 1.25,
+  wheat: 1.25,
+  rosemary: 1.5,
+  french: 1.5,
+  ciabatta: 1.5,
+};
+
+const getBread = () => {
+  return bread
+};
+
+export default { getBread }
---
+++ javascripts/components/meat.js
@@ -0,0 +1,13 @@
+const meat = {
+  turkey: 1.25,
+  salami: 1.5,
+  ham: 1.25,
+  chicken: 1.25,
+  pepperoni: 1.5,
+};
+
+const getMeat = () => {
+  return meat
+};
+
+export default { getMeat }
