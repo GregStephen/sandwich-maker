d5506a473ab11256544015f2ec4b52fffae29bd4
Greg Stephen <GregAStephen@gmail.com>
2019-04-13 08:07:20 -0500

    WIP

--- javascripts/components/sammie_menu.js
+++ javascripts/components/sammie_menu.js
@@ -14,14 +14,38 @@ const makeMenu = () => {
   const cheeseInfo = Object.entries(cheeseObj)
   const breadObj = bread.getBread();
   const breadInfo = Object.entries(breadObj)
-  let domString = '';
+  let meatString = '';
+  let veggieString = '';
+  let cheeseString = '';
+  let breadString = '';
   for (const [meat, price] of meatInfo) {
-    domString += `<div class="menuCheckbox">`
-    domString += `<input type="checkbox" name="meat" id="${meat}" value="${meat}">`
-    domString += `<label for="${meat}">${meat}: $${price}</label>`
-    domString += `</div>`
-  }
-  util.printToDom('meats', domString);
+    meatString += `<div class="menuCheckbox">`
+    meatString += `<input type="checkbox" name="meat" id="${meat}" value="${meat}">`
+    meatString += `<label for="${meat}">${meat}: $${price}</label>`
+    meatString += `</div>`
+  };
+  for (const [veggie, price] of veggieInfo) {
+    veggieString += `<div class="menuCheckbox">`
+    veggieString += `<input type="checkbox" name="veggie" id="${veggie}" value="${veggie}">`
+    veggieString += `<label for="${veggie}">${veggie}: $${price}</label>`
+    veggieString += `</div>`
+  };
+  for (const [cheese, price] of cheeseInfo) {
+    cheeseString += `<div class="menuCheckbox">`
+    cheeseString += `<input type="checkbox" name="cheese" id="${cheese}" value="${cheese}">`
+    cheeseString += `<label for="${cheese}">${cheese}: $${price}</label>`
+    cheeseString += `</div>`
+  };
+  for (const [bread, price] of breadInfo) {
+    breadString += `<div class="menuCheckbox">`
+    breadString += `<input type="radio" name="bread" id="${bread}" value="${bread}">`
+    breadString += `<label for="${bread}">${bread}: $${price}</label>`
+    breadString += `</div>`
+  };
+  util.printToDom('meats', meatString);
+  util.printToDom('veggies', veggieString);
+  util.printToDom('cheese', cheeseString);
+  util.printToDom('bread', breadString);
   events.attachEvents();
 };
 
--- javascripts/helpers/eventHandlers.js
+++ javascripts/helpers/eventHandlers.js
@@ -1,28 +1,64 @@
 import util from './util.js';
+import bread from '../components/bread.js';
+import meat from '../components/meat.js';
+import veggies from '../components/veggie.js';
+import cheese from '../components/cheese.js';
 
 const meatCheckboxes = document.getElementsByName('meat');
-let ingredientList = [];
+const veggieCheckboxes = document.getElementsByName('veggie');
+const cheeseCheckboxes = document.getElementsByName('cheese');
+const breadRadioBtns = document.getElementsByName('bread');
+let meatIngredientList = [];
+let veggieIngredientList = [];
+let cheeseIngredientList = [];
+// let ingredientList = [];
+let breadChoice = '';
 
-
-const checkboxChange = (e) => {
+const checkboxChange = (e, type) => {
   let checkbox = e.target;
   let checkboxId = e.target.id;
+  let checkboxType = e.target.name;
+  console.log(checkboxType);
+  console.log("type", type);
+  let ingredientList = `${type}IngredientList`;
+  ingredientList = [];
+  let div = `${type}-choice-div`;
 
-  if (checkbox.checked) {
-    ingredientList.push(checkboxId);
-    util.printToDom('finished-sandwich-div', ingredientList);
-  } else {
-    let index = ingredientList.indexOf(checkboxId);
-    if (index !== -1) {
-      ingredientList.splice(index, 1);
+  if (checkboxType === type){
+    if (checkbox.checked) {
+      ingredientList.push(checkboxId);
+      util.printToDom(div, ingredientList);
+    } else {
+      let index = ingredientList.indexOf(checkboxId);
+      if (index !== -1) {
+        ingredientList.splice(index, 1);
+      }
+      util.printToDom(div, ingredientList);
     }
-    util.printToDom('finished-sandwich-div', ingredientList);
   }
 };
 
+const radioChange = (e) => {
+  let radio = e.target;
+  let radioId = e.target.id;
+  if (radio.checked) {
+    breadChoice = `${radioId} bread`;
+  }
+  util.printToDom('bread-choice-div', breadChoice);
+}
+
 const attachEvents = () => {
   for (const meatCheckbox of meatCheckboxes) {
-    meatCheckbox.addEventListener('change', checkboxChange);
+    meatCheckbox.addEventListener('change', (event) => { checkboxChange(event, "meat"); });
+  }
+  for (const veggieCheckbox of veggieCheckboxes) {
+    veggieCheckbox.addEventListener('change', (event) => { checkboxChange(event, "veggie"); });
+  }
+  for (const cheeseCheckbox of cheeseCheckboxes) {
+    cheeseCheckbox.addEventListener('change', (event) => { checkboxChange(event, "cheese"); });
+  }
+  for (const breadRadioBtn of breadRadioBtns) {
+    breadRadioBtn.addEventListener('change', radioChange);
   }
 }
 
