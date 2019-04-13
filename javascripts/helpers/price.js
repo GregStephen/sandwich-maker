012e966e9395445249da17a49ff6a4d191ac9c9e
Greg Stephen <GregAStephen@gmail.com>
2019-04-13 10:27:37 -0500

    did some cleanup, more needed

--- javascripts/helpers/price.js
+++ javascripts/helpers/price.js
@@ -5,40 +5,28 @@ import cheese from '../components/cheese.js';
 
 
 const addMeat = (meatType) => {
-  console.log("meatType",meatType);
   let meatObj = meat.getMeat();
-  console.log("meatObj", meatObj)
   let price = meatObj[meatType];
-  console.log("price", price)
   return price;
 };
-
 
 const addVeggie = (veggieType) => {
-
   let veggieObj = veggies.getVeggie();
-  
   let price = veggieObj[veggieType];
-  
   return price;
 };
 
-
 const addCheese = (cheeseType) => {
-
   let cheeseObj = cheese.getCheese();
-  
   let price = cheeseObj[cheeseType];
-  
   return price;
 };
 
 const addBread = (breadType) => {
-
   let breadObj = bread.getBread();
-  
   let price = breadObj[breadType];
-  
   return price;
 };
+
+
 export default { addMeat, addVeggie, addCheese, addBread }
